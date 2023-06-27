5e4c655ef9801ed64979d0d5959c21035b0d1def
GISOjinga <abuchiaaa@gmail.com>
2023-06-27 10:43:07 -0400

    done

--- src/app.py
+++ src/app.py
@@ -1,39 +1,53 @@
-# add the jsonify method to your Flask import
+# Import necessary modules from flask
 from flask import Flask, jsonify, request
+
+# Create a new Flask web server
 app = Flask(__name__)
 
-# suppose you have your data in the variable named some_data
+# Initialize a list of todos
 todos = [
-   { "label": "My first task", "done": False }
+    {
+        "done": True,
+        "label": "Sample Todo 1"
+    },
+    {
+        "done": True,
+        "label": "Sample Todo 2"
+    }
 ]
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    # you can convert that variable into a json string like this
-    json_text = jsonify(todos)
 
-    # and then you can return it to the front end in the response body like this
-    return json_text
-
-
-@app.route('/todos', methods=['POST'])
-def add_new_todo():
-    request_body = request.json
-    print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
-
-
-
-
-@app.route('/todos/<int:position>', methods=['DELETE'])
-def delete_todo(position):
-    #removes it at that position
-    todos.remove(position)
-    # returns the string version
+# Define the route for the URL "/todos" to get all todos
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    # Convert the todos list into JSON and return it as a response
     return jsonify(todos)
 
 
 
-# These two lines should always be at the end of your app.py file.
+# Define the route for the URL "/todos" to add a new todo
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    # Append a new todo at the end of the todos list
+    todos.append({
+        "done": False,  # The new todo is not done yet
+        "label": "Sample Todo " + str(len(todos) + 1)  # The label of the new todo
+    })
+    # Convert the updated todos list into JSON and return it as a response
+    return jsonify(todos)
+
+
+
+# Define the route for the URL "/todos/<position>" to delete a todo at a specific position
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    # Check if the position is within the range of the todos list
+    if position < len(todos):
+        # If it is, remove the todo at that position
+        todos.pop(position)
+        # Convert the updated todos list into JSON and return it as a response
+        return jsonify(todos)
+
+# Run the Flask web server if this file is the main program
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
